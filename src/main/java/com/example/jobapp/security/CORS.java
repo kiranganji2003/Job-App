5160e550aabc4226b3b505e248ef29ba1cbd7562
Kiran Ganji <kiranganji2003@gmail.com>
2025-08-05 23:55:04 +0530

    CORS added

---
+++ src/main/java/com/example/jobapp/security/CORS.java
@@ -0,0 +1,20 @@
+package com.example.jobapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORS {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+}
